a86da20f7b598ac045f66d813d90935d24f06d1d
Rotimi <rotimiolaolu@gmail.com>
2024-11-20 12:12:30 +0100

    update

--- service/baseUrl.tsx
+++ service/baseUrl.tsx
@@ -6,8 +6,7 @@ const BaseUrlApi = axios.create({
   baseURL: baseURL,
   headers: {
     accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MDJiN2FjYjhjNmVlZDBhZjgzNzUxMDEwYTUxYTg4MCIsIm5iZiI6MTczMTkyNzQ1OC40NTM1MDQ4LCJzdWIiOiI2M2MzYjQzYmFhZWM3MTAwOTQ2N2M1NTMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.zIf503kFLUIrqIg70FUunOH4NWr7Pn8d-jRthr492Lg",
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_KEY}`,
   },
 });
 
